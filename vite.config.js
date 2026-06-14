d9fbab762387be181f53627149374a63edf71244
Nelly Delgado Planche <ndplanche@gmail.com>
2026-06-14 11:07:26 -0400

    create vite multi page build

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,16 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
-  base: "/PawMatch/"
+  base: "/PawMatch/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        quiz: resolve(__dirname, "quiz.html"),
+        results: resolve(__dirname, "results.html"),
+        breedDetail: resolve(__dirname, "breed-detail.html")
+      }
+    }
+  }
 });
